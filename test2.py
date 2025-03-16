1ab7158f01ca442f5979b737d724b3725aa7281c
jogeswarnanda <44108715+jogeswarnanda@users.noreply.github.com>
2025-03-17 00:08:24 +0530

    hiii

--- test2.py
+++ test2.py
@@ -1,2 +1,8 @@
-print("python111")
-print("python >> Updated in my mac !!")
+sal =(input("Enter your salary: "))
+sal = int(sal)
+tax=sal*(0.1,0.2)[sal>50000]
+print("Tax to be paid: ",tax)
+print("Tax to be paid: ",tax)
+sysint1 = int(sal)
+sysint1 = sysint1+1
+print(sysint1)...
